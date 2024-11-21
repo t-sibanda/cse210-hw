cce647d05589da5695f0ab6e6d2c9a8d024343d7
tisibanda <tsibanda@usapglobal.org>
2024-11-21 13:39:19 -0500

    week02 abstract assignment complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,10 +6,14 @@ class Program
     {
         
         // Create a new job.
-        Job job1 = new Job("Facilities Manager", "Dawn Properties", 2021, 2022);
+        Job job1 = new Job("Projects Engineer", "Techniplas LLC", 2023, 2024);
 
         // Create a second job.
-        Job job2 = new Job("Account Manager", "Vivo Energy", 2020, 2021);
+        Job job2 = new Job("Facilities Manager", "Dawn Properties Pvt Ltd", 2021, 2022);
+
+        // Create a third job.
+        Job job3 = new Job("Account Manager", "Vivo Energy", 2020, 2021);
+     
 
         // Create a new resume.
         Resume resume = new Resume("Terrence Sibanda");
@@ -17,6 +21,7 @@ class Program
         // Add the jobs to the resume.
         resume.AddJob(job1);
         resume.AddJob(job2);
+        resume.AddJob(job3);
 
         // Display the resume.
         resume.Display();
