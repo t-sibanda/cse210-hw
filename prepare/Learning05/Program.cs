580fd693e42bc1df95c3e63333759d7ee8f2bb4f
tisibanda <tsibanda@usapglobal.org>
2024-12-16 19:21:09 -0500

    Inheritence Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment basicAssignment = new Assignment("Laangah Atida", "Addition and Subtraction");
+        Console.WriteLine(basicAssignment.GetSummary());
+        
+        MathAssignment mathAssignment = new MathAssignment("Terry Sibanda", "Calculus", "9.3", "18-29");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        
+        WritingAssignment writingAssignment = new WritingAssignment("Nolu Tsitsi", "European History", "The Sarajevo Incident");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
